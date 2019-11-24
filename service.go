d516bb999726f469be1a206bbd61e24654e5a33b
konorlevich <konorlevich@gmail.com>
2019-11-24 17:25:58 +0300

    feat: add log streaming

--- service.go
+++ service.go
@@ -7,10 +7,12 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/peer"
 	"google.golang.org/grpc/status"
 	"log"
 	"net"
 	"strings"
+	"sync"
 )
 
 type MyBizServer struct {
@@ -24,6 +26,27 @@ func (*MyBizServer) Add(ctx context.Context, req *Nothing) (*Nothing, error) {
 }
 func (*MyBizServer) Test(ctx context.Context, req *Nothing) (*Nothing, error) {
 	return &Nothing{}, nil
+}
+
+type MyAdminServer struct {
+	LogChan chan Event
+	clients []chan Event
+	mu      *sync.RWMutex
+}
+
+func (as *MyAdminServer) Logging(req *Nothing, srv Admin_LoggingServer) error {
+	lc := make(chan Event)
+	as.mu.Lock()
+	as.clients = append(as.clients, lc)
+	as.mu.Unlock()
+	for event := range lc {
+		srv.Send(&event)
+	}
+	return nil
+}
+
+func (*MyAdminServer) Statistics(req *StatInterval, srv Admin_StatisticsServer) error {
+	return status.Errorf(codes.Unimplemented, "method Statistics not implemented")
 }
 
 func StartMyMicroservice(ctx context.Context, addr string, data string) error {
@@ -45,8 +68,50 @@ func StartMyMicroservice(ctx context.Context, addr string, data string) error {
 	return nil
 }
 
+func gRPCService(addr string, aclData string) (func(), error) {
+	auth, err := newAuth(aclData)
+	if err != nil {
+		return nil, err
+	}
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalln("failed to listen TCP port", err)
+	}
+
+	server := grpc.NewServer(
+		grpc.UnaryInterceptor(auth.authInterceptor),
+		grpc.StreamInterceptor(auth.streamInterceptor),
+	)
+	adminSrv := new(MyAdminServer)
+	adminSrv.LogChan = auth.Log
+	adminSrv.mu = &sync.RWMutex{}
+	RegisterAdminServer(server, adminSrv)
+	RegisterBizServer(server, new(MyBizServer))
+
+	go func() {
+		for event := range adminSrv.LogChan {
+			adminSrv.mu.Lock()
+			clients := adminSrv.clients
+			adminSrv.mu.Unlock()
+			for _, client := range clients {
+				client <- event
+			}
+		}
+	}()
+
+	go server.Serve(lis)
+	return func() {
+		close(auth.Log)
+		for _, client := range adminSrv.clients {
+			close(client)
+		}
+		server.GracefulStop()
+	}, nil
+}
+
 func newAuth(aclData string) (*Auth, error) {
 	auth := Auth{}
+	auth.Log = make(chan Event, 5)
 	err := json.Unmarshal([]byte(aclData), &auth.AclData)
 	if err != nil {
 		return nil, errors.New("expacted error on bad acl json, have nil")
@@ -55,6 +120,7 @@ func newAuth(aclData string) (*Auth, error) {
 }
 
 type Auth struct {
+	Log     chan Event
 	AclData map[string][]string
 }
 
@@ -81,6 +147,18 @@ func (a *Auth) checkAuth(ctx context.Context, method string) bool {
 	return false
 }
 
+func (a *Auth) pushEventToLog(ctx context.Context, methodName string) {
+
+	md, _ := metadata.FromIncomingContext(ctx)
+	consumer := md.Get("consumer")
+	p, _ := peer.FromContext(ctx)
+	event := Event{}
+	event.Consumer = consumer[0]
+	event.Method = methodName
+	event.Host = p.Addr.String()
+	a.Log <- event
+}
+
 func (a *Auth) authInterceptor(
 	ctx context.Context,
 	req interface{},
@@ -90,6 +168,7 @@ func (a *Auth) authInterceptor(
 	if !a.checkAuth(ctx, info.FullMethod) {
 		return nil, status.Error(codes.Unauthenticated, "")
 	}
+	a.pushEventToLog(ctx, info.FullMethod)
 	return handler(ctx, req)
 }
 
@@ -102,29 +181,6 @@ func (a *Auth) streamInterceptor(
 	if !a.checkAuth(ss.Context(), info.FullMethod) {
 		return status.Error(codes.Unauthenticated, "")
 	}
+	a.pushEventToLog(ss.Context(), info.FullMethod)
 	return handler(srv, ss)
-}
-
-func gRPCService(addr string, aclData string) (func(), error) {
-	auth, err := newAuth(aclData)
-	if err != nil {
-		return nil, err
-	}
-	lis, err := net.Listen("tcp", addr)
-	if err != nil {
-		log.Fatalln("failed to listen TCP port", err)
-	}
-
-	server := grpc.NewServer(
-		grpc.UnaryInterceptor(auth.authInterceptor),
-		grpc.StreamInterceptor(auth.streamInterceptor),
-	)
-
-	RegisterAdminServer(server, new(UnimplementedAdminServer))
-	RegisterBizServer(server, new(MyBizServer))
-
-	go server.Serve(lis)
-	return func() {
-		server.GracefulStop()
-	}, nil
 }
